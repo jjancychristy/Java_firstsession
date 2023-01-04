4e283096a3a990e69702b41247b63db61f0dbf60
jjancychristy <jjancychristy24081988@gmail.com>
2023-01-04 14:00:11 -0500

    2nd commit

--- src/Jan03/ControlStatement.java
+++ src/Jan03/ControlStatement.java
@@ -5,13 +5,13 @@ public class ControlStatement {
 		// TODO Auto-generated method stub
 int a=10;
 int b=20;
-if(b==a)
+if(a>b)
+{
+	System.out.println("a is greater");
+}
+else if(a<b)
 {
 	System.out.println("b is greater");
-}
-else if(b<a)
-{
-	System.out.println("b is correct");
 	}
 else
 {
